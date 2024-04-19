26d9e61efd2ed1a49a91996b5c7222971ac2c3b3
huyngse <huyngse173222@fpt.edu.vn>
2024-04-19 17:54:33 +0700

    Portfolio many to many

--- api/Data/ApplicationDbContext.cs
+++ api/Data/ApplicationDbContext.cs
@@ -16,10 +16,22 @@ namespace api.Data
 
         public DbSet<Stock> Stocks { get; set; }
         public DbSet<Comment> Comments { get; set; }
+        public DbSet<Portfolio> Portfolios { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+            builder.Entity<Portfolio>(x => x.HasKey(p => new { p.AppUserId, p.StockId }));
+            builder
+                .Entity<Portfolio>()
+                .HasOne(x => x.AppUser)
+                .WithMany(x => x.Portfolios)
+                .HasForeignKey(x => x.AppUserId);
+            builder
+                .Entity<Portfolio>()
+                .HasOne(x => x.Stock)
+                .WithMany(x => x.Portfolios)
+                .HasForeignKey(x => x.StockId);
             List<IdentityRole> roles = new List<IdentityRole>
             {
                 new IdentityRole { Name = "Admin", NormalizedName = "ADMIN" },
